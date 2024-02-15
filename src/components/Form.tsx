5ddf7fa839d6b25d710a64e2450e868463ba18b8
axunion <120321953+axunion@users.noreply.github.com>
2024-02-15 23:36:23 +0900

    Isolate Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,90 @@
+import { type Component, onMount } from "solid-js";
+
+const keyframes = {
+  fadeIn: [{ opacity: 0 }, { opacity: 1 }],
+  fadeOut: [{ opacity: 1 }, { opacity: 0 }],
+  slideUp: [
+    { opacity: 0, transform: "translateY(20px)" },
+    { opacity: 1 },
+    { opacity: 1, transform: "translateY(0)" },
+  ],
+  slideDown: [
+    { opacity: 1, transform: "translateY(0)" },
+    { opacity: 1 },
+    { opacity: 0, transform: "translateY(20px)" },
+  ],
+};
+
+const options = {
+  fadeIn: { duration: 200 },
+  fadeOut: { duration: 400 },
+  slideUp: { duration: 250 },
+  slideDown: { duration: 400 },
+};
+
+const Form: Component<{
+  showForm: boolean;
+  setShowForm: (value: boolean) => void;
+}> = (props) => {
+  let overlay: HTMLDivElement | undefined;
+  let form: HTMLFormElement | undefined;
+
+  onMount(() => {
+    overlay!.animate(keyframes.fadeIn, options.fadeIn);
+    form!.animate(keyframes.slideUp, options.slideUp);
+  });
+
+  const close = () => {
+    const animation = overlay!.animate(keyframes.fadeOut, options.fadeOut);
+    form!.animate(keyframes.slideDown, options.slideDown);
+
+    animation.onfinish = () => props.setShowForm(false);
+  };
+
+  const stopPropagation = (e: Event) => {
+    e.stopPropagation();
+  };
+
+  const submit = (e: Event) => {
+    e.preventDefault();
+    console.log("submit");
+    close();
+  };
+
+  return (
+    <div
+      ref={overlay}
+      class="bg-black/50 backdrop-blur-sm fixed inset-0 flex flex-col justify-center items-center p-4"
+      onClick={close}
+    >
+      <form
+        ref={form}
+        class="max-w-screen-sm w-full"
+        onClick={stopPropagation}
+        onSubmit={submit}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="お名前"
+          class="bg-[--color-background] rounded-lg p-4 w-full"
+        />
+
+        <textarea
+          name="content"
+          placeholder="祈りの課題"
+          class="bg-[--color-background] rounded-lg p-4 mt-2 w-full h-80 resize-none"
+        ></textarea>
+
+        <button
+          type="submit"
+          class="bg-[--color-accent] text-white rounded-lg p-4 mt-2 w-full"
+        >
+          送信
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
